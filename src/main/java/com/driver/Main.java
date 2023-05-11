2f91fd58206257ac211ac250b54cc1f06831a3db
prasadpatil079 <prasadpatil079@gmail.com>
2023-05-11 13:01:38 +0530

    updated assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Sam");
+        String name= obj.getName();
+        System.out.print(name);
+    }//error cannot find Symbol
 }
